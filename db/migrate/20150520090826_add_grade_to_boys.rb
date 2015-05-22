9548f1c5291053c7428d4ec5eda2d08129bedeb4
Merovex <dausha@gmail.com>
2015-05-22 10:44:39 -0400

    Added Grades and ability to promote.

--- db/migrate/20150520090826_add_grade_to_boys.rb
+++ db/migrate/20150520090826_add_grade_to_boys.rb
@@ -1,6 +1,6 @@
 class AddGradeToBoys < ActiveRecord::Migration
   def change
   	add_column :boys, :grade, :integer, :default => 0
-  	add_column :boys, :active, :boolean, :default => 1
+  	add_column :boys, :active, :boolean, :default => true
   end
 end
