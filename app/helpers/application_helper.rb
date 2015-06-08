9fd91d14e8c1f868d9e30497a134b03f834ef7a2
Merovex <dausha@gmail.com>
2015-06-08 06:08:34 -0400

    Branches and pins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,11 +27,11 @@ module ApplicationHelper
       when n > (total + 4) then
         x = ((n - total) / 4).floor
         # return "<span class='bg-danger' style='padding: 2px'><abbr title='#{n} /#{total} leaves'><strong>#{x}</strong> star#{x==1 ? '' : 's'}</abbr></span>".html_safe;
-        return "<span class='bg-success' style='padding: 2px'><abbr title='#{n}/#{total} leaves'><strong>Branch + #{x}</strong></abbr></span>".html_safe;
+        return "<span class='bg-success' style='padding: 2px'><abbr title='#{n}/#{total} leaves'><strong>B + #{x}</strong></abbr></span>".html_safe;
       when n < total
         return "<span><abbr title='#{(n.to_f / total.to_f * 100.0).to_i} %'><strong>#{n}</strong><small class='text-muted'>/#{total}</small></abbr></span>".html_safe;
       else
-        return "<span class='bg-success' style='padding: 2px'><abbr title='#{n}/#{total} leaves'><strong>Branch</strong></abbr></span>".html_safe;
+        return "<span class='bg-success' style='padding: 2px'><abbr title='#{n}/#{total} leaves'><strong>B</strong></abbr></span>".html_safe;
     end
     
   end
