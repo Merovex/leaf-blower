31d81433992010c84a00cfdf37c2c84b5ae74842
Merovex <dausha@gmail.com>
2015-07-21 21:30:38 -0400

    Finalized.

--- db/migrate/20150721100438_fix_role_to_users.rb
+++ db/migrate/20150721100438_fix_role_to_users.rb
@@ -2,9 +2,9 @@ class FixRoleToUsers < ActiveRecord::Migration
   def change
   	change_table :users do |t|
 	  # t.change :role, :integer, default: 0
-	  t.change :role, :integer, 'integer USING CAST(column_name AS integer)'
+	  t.change :role, :integer, 'integer USING CAST(role AS integer)'
 	end
-	User.reset_column_information
+	# User.reset_column_information
 	# User.update_all(role: 0)
   end
 end
