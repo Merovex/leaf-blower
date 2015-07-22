f4f78ee18144b33ced8346232a73e6e46c4170ae
Merovex <dausha@gmail.com>
2015-07-21 21:22:23 -0400

    Finalized.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
-  # enum role: [:user, :vip, :admin]
+  
   enum role: [:nobody, :folk, :leader, :ranger]
-  # enum status: [:draft, :review, :active, :retired]
-  # after_initialize :set_default_role, :if => :new_record?
+  
+  after_initialize :set_default_role, :if => :new_record?
 
-  ROLES = %w[ranger leader parent user banned]
+  # ROLES = %w[ranger leader parent user banned]
 
-  # def set_default_role
-  #   self.role ||= :user
-  # end
-  # def role?(r)
-  #   m = (self.role.nil?) ? 'Nobody' : self.role.to_sym
-  # 	(m == r)
-  # end
+  def set_default_role
+    self.nobody!
+  end
 
 end
