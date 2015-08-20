25c4dc40e182115691cbeb3d3636a4336d0c6f21
Merovex <dausha@gmail.com>
2015-08-20 05:38:31 -0400

    Removing confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable, #:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   
   enum role: [:nobody, :folk, :leader, :ranger]
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   # ROLES = %w[ranger leader parent user banned]
 
   def set_default_role
-    self.nobody!
+    # self.nobody!
   end
 
 end
