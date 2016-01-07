1579bf6b117f073072a63f6c50964568b1702398
Merovex <dausha@gmail.com>
2016-01-06 19:21:25 -0500

    Resolving uncommitted

---
+++ spec/features/patrols/create_patrol_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper.rb'
+
+feature 'Creating patrols' do  
+  scenario 'can create a patrol' do
+  	visit '/'
+    # visit '/patrols'
+    # click_link 'New Patrol'
+    # attach_file('Image', "spec/files/images/coffee.jpg")
+    # fill_in 'Caption', with: 'nom nom nom #coffeetime' 
+    # click_button 'Create Post'
+    # expect(page).to have_content('#coffeetime')
+    # expect(page).to have_css("img[src*='coffee.jpg']")
+  end
+end  
